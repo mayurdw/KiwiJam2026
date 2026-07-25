892a479462a60399931c8593e95518923d517e15
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2026-07-25 17:51:07 +1200

    Countdown

--- scenes/game_scene/levels/level.gd
+++ scenes/game_scene/levels/level.gd
@@ -11,12 +11,16 @@ signal level_changed(level_path : String)
 @export var tasks: Array[String] = []
 @export var aim: String = ""
 @export var index: int = 0
+@export var remaining_time_sec : int = 60
 
-@onready var element_handler: ElementHandler = $MarginContainer/HBoxContainer/VBoxContainer/MarginContainer/ElementHandler
-@onready var goal: Label = $"MarginContainer/HBoxContainer/Task Sidebar/Goal Container/MarginContainer/VBoxContainer3/Goal"
-@onready var next_task: Label = $"MarginContainer/HBoxContainer/Task Sidebar/MarginContainer/Tasks Container/PanelContainer3/MarginContainer/VBoxContainer5/Next Task"
-@onready var current_task: Label = $"MarginContainer/HBoxContainer/Task Sidebar/MarginContainer/Tasks Container/PanelContainer2/MarginContainer/VBoxContainer4/Current Task"
+@onready var element_handler: ElementHandler = $VBoxContainer/MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/MarginContainer/ElementHandler
+@onready var goal: Label = $"VBoxContainer/MarginContainer/HBoxContainer/MarginContainer2/Task Sidebar/Goal Container/MarginContainer/VBoxContainer3/Goal"
+@onready var current_task: Label = $"VBoxContainer/MarginContainer/HBoxContainer/MarginContainer2/Task Sidebar/MarginContainer/Tasks Container/PanelContainer2/MarginContainer/VBoxContainer4/Current Task"
+@onready var next_task: Label = $"VBoxContainer/MarginContainer/HBoxContainer/MarginContainer2/Task Sidebar/MarginContainer/Tasks Container/PanelContainer3/MarginContainer/VBoxContainer5/Next Task"
+@onready var label: Label = $VBoxContainer/MarginContainer2/HBoxContainer/Label
+@onready var progress_bar: ProgressBar = $VBoxContainer/MarginContainer2/HBoxContainer/ProgressBar
 @onready var thread: Line2D = $Thread
+@onready var timer: Timer = $Timer
 
 var level_state : LevelState
 
@@ -39,8 +43,14 @@ func _ready() -> void:
 	goal.text = aim
 	current_task.text = aim
 	next_task.text = aim
+	timer.start()
+	label.text = "%d:%d" % [ remaining_time_sec / 60, remaining_time_sec % 60]
+	progress_bar.max_value = remaining_time_sec + level_state.carry_over_time
+	progress_bar.value = progress_bar.max_value
 
 func _complete_level() -> void:
+	timer.stop()
+	level_state.carry_over_time = remaining_time_sec
 	level_won.emit()
 
 func _on_grid_container_button_pressed(current_value: int, current_position: Vector2) -> void:
@@ -59,3 +69,14 @@ func _on_grid_container_button_pressed(current_value: int, current_position: Vec
 		points.append(current_position)
 
 	thread.points = points
+
+
+func _on_timer_timeout() -> void:
+	remaining_time_sec -= 1
+	if remaining_time_sec <= 0:
+		level_lost.emit()
+	else:
+		print("Time Remaining %d" % remaining_time_sec)
+		progress_bar.value = remaining_time_sec
+		label.text = "0:%d" % remaining_time_sec
+		timer.start()
